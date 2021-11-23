64a3204ea9c952db32bd3d1db1ad1929f567aca5
sudhir4365 <33348299+sudhir4365@users.noreply.github.com>
2021-11-23 12:33:13 +0530

    guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+echo "[Welcome to Guessing Game.]"
+
+function Guess {
+	echo "Please Guess the number of files in current directory:"
+	read guess
+    files=$(ls -1 | wc -l)
+    
+}
+
+Guess
+
+while [[ $guess -ne $files ]]
+do
+	if [[ $guess -lt $files ]] 
+	then
+		echo "Please select a higher Number."
+	else
+		echo "Please select a lower Number."
+	fi
+	Guess
+done
+
+echo "Congratulation! That's the correct answer. You have done it. Here is the list: "
+echo " " && ls -1
